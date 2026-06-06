3ea0be477b117124b060d2650743764e459111c2
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2026-06-05 20:32:56 -0700

    feat: Web Worker wrapper for SA trajectory optimizer

---
+++ src/solver/worker.js
@@ -0,0 +1,39 @@
+import { buildStairwell } from '../geometry/stairwell.js';
+import { getHalfExtents } from '../geometry/box.js';
+import { buildCenterline } from './path.js';
+import { optimizeTrajectory } from './trajectory.js';
+
+let cancelFlag = false;
+
+self.onmessage = async ({ data }) => {
+  if (data.type === 'cancel') {
+    cancelFlag = true;
+    return;
+  }
+
+  if (data.type === 'start') {
+    cancelFlag = false;
+    const { stairwellParams, boxDims, weights } = data;
+
+    const { collisionQuads } = buildStairwell(stairwellParams);
+    const halfExtents        = getHalfExtents(boxDims);
+    const centerline         = buildCenterline(stairwellParams);
+
+    const result = await optimizeTrajectory(
+      collisionQuads,
+      halfExtents,
+      centerline,
+      weights,
+      (progress) => {
+        self.postMessage({ type: 'progress', ...progress });
+      },
+      () => cancelFlag,
+    );
+
+    if (cancelFlag) {
+      self.postMessage({ type: 'canceled', ...result });
+    } else {
+      self.postMessage({ type: 'done', ...result });
+    }
+  }
+};
