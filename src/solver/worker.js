ba83dc27a9ff5440f7920eab8774a8996e8f0b93
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2026-06-05 20:34:28 -0700

    fix: catch errors in worker start handler to prevent silent worker death

--- src/solver/worker.js
+++ src/solver/worker.js
@@ -14,26 +14,29 @@ self.onmessage = async ({ data }) => {
   if (data.type === 'start') {
     cancelFlag = false;
     const { stairwellParams, boxDims, weights } = data;
+    try {
+      const { collisionQuads } = buildStairwell(stairwellParams);
+      const halfExtents        = getHalfExtents(boxDims);
+      const centerline         = buildCenterline(stairwellParams);
 
-    const { collisionQuads } = buildStairwell(stairwellParams);
-    const halfExtents        = getHalfExtents(boxDims);
-    const centerline         = buildCenterline(stairwellParams);
+      const result = await optimizeTrajectory(
+        collisionQuads,
+        halfExtents,
+        centerline,
+        weights,
+        (progress) => {
+          self.postMessage({ type: 'progress', ...progress });
+        },
+        () => cancelFlag,
+      );
 
-    const result = await optimizeTrajectory(
-      collisionQuads,
-      halfExtents,
-      centerline,
-      weights,
-      (progress) => {
-        self.postMessage({ type: 'progress', ...progress });
-      },
-      () => cancelFlag,
-    );
-
-    if (cancelFlag) {
-      self.postMessage({ type: 'canceled', ...result });
-    } else {
-      self.postMessage({ type: 'done', ...result });
+      if (cancelFlag) {
+        self.postMessage({ type: 'canceled', ...result });
+      } else {
+        self.postMessage({ type: 'done', ...result });
+      }
+    } catch (err) {
+      self.postMessage({ type: 'error', message: err.message });
     }
   }
 };
